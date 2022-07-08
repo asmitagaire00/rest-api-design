0530b5f18d2ed0c38f18af8c711d344262e281d7
Asmita Gaire <asmitagaire00@gmail.com>
2022-07-08 17:46:33 +1000

    create service layer and update controller

--- src/controllers/workoutController.js
+++ src/controllers/workoutController.js
@@ -1,20 +1,28 @@
+const workoutService = require("../services/workoutService");
+
 const getAllWorkout = (req, res) => {
+  const allWorkout = workoutService.getAllWorkout();
   res.send("Get all workouts");
 };
 
 const getOneWorkout = (req, res) => {
+  const oneWorkout = workoutService.getOneWorkout();
   res.send("get one workout");
 };
 
 const createNewWorkout = (req, res) => {
+  const newWorkout = workoutService.createNewWorkout();
   res.send("post new workout");
 };
 
 const updateOneWorkout = (req, res) => {
+  const updatedWorkout = workoutService.updateOneWorkout();
   res.send("update one workout");
 };
 
 const deleteOneWorkout = (req, res) => {
+  const allWorkout = workoutService.deleteOneWorkout();
+
   res.send("delete one workout");
 };
 
---
+++ src/services/workoutService.js
@@ -0,0 +1,23 @@
+const getAllWorkout = () => {
+  return;
+};
+const getOneWorkout = () => {
+  return;
+};
+const createNewWorkout = () => {
+  return;
+};
+const updateOneWorkout = () => {
+  return;
+};
+const deleteOneWorkout = () => {
+  return;
+};
+
+module.exports = {
+  getAllWorkout,
+  getOneWorkout,
+  createNewWorkout,
+  updateOneWorkout,
+  deleteOneWorkout,
+};
